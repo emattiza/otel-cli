ea40f2657cbcbbd0f1fe5121884a34f259d4e455
Evan Mattiza <emattiza@gmail.com>
2024-03-16 01:00:23 -0500

    update: 0.4.1 -> 0.4.4

--- contrib/application.nix
+++ contrib/application.nix
@@ -1,19 +1,28 @@
-{ lib, buildGoModule, fetchFromGitHub, getent, coreutils, nix-update-script, stdenv }:
-
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+  getent,
+  coreutils,
+  nix-update-script,
+  stdenv,
+}:
 buildGoModule rec {
   pname = "otel-cli";
-  version = "0.4.1";
+  version = "0.4.4";
 
   src = ../.;
 
   vendorHash = "sha256-HwbEqWtOqiTe5Z/MtMAs63Lzvll/vgmbCpMTREXgtXA=";
 
-  preCheck = ''
-    ln -s $GOPATH/bin/otel-cli .
-  '' + lib.optionalString (!stdenv.isDarwin) ''
-    substituteInPlace main_test.go \
-      --replace 'const minimumPath = `/bin:/usr/bin`' 'const minimumPath = `${lib.makeBinPath [ getent coreutils ]}`'
-  '';
+  preCheck =
+    ''
+      ln -s $GOPATH/bin/otel-cli .
+    ''
+    + lib.optionalString (!stdenv.isDarwin) ''
+      substituteInPlace main_test.go \
+        --replace 'const minimumPath = `/bin:/usr/bin`' 'const minimumPath = `${lib.makeBinPath [getent coreutils]}`'
+    '';
 
   passthru.updateScript = nix-update-script {};
 
@@ -22,7 +31,7 @@ buildGoModule rec {
     description = "A command-line tool for sending OpenTelemetry traces";
     changelog = "https://github.com/equinix-labs/otel-cli/releases/tag/v${version}";
     license = licenses.asl20;
-    maintainers = with lib.maintainers; [ emattiza urandom ];
+    maintainers = with lib.maintainers; [emattiza urandom];
     mainProgram = "otel-cli";
   };
 }
